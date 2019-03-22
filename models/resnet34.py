4e1d94fb332a261c36e4fb2d60c1a4ff9fe53136
Tamer Thamoqa <tamerthamoqa@gmail.com>
2019-03-22 16:49:59 +0200

    Add resnet34 architecture

---
+++ models/resnet34.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+import torchvision.models as models
+
+
+class Resnet34(nn.Module):
+    """Constructs a ResNet-34 model for FaceNet training using center loss with softmax loss.
+
+    Args:
+        num_classes (int): Number of classes in the training dataset required for softmax loss.
+        embedding_dimension (int): Required dimension of the resulting embedding layer that is outputted by the model.
+                                   using center loss. Defaults to 128.
+        pretrained (bool): If True, returns a model pre-trained on the ImageNet dataset from a PyTorch repository.
+                           Defaults to False.
+    """
+    def __init__(self, num_classes, embedding_dimension=128, pretrained=False):
+        super(Resnet34, self).__init__()
+        self.model = models.resnet34(pretrained=pretrained)
+        self.embedding_dimension = embedding_dimension
+        input_features_fc_layer = self.model.fc.in_features
+        # Output embedding
+        self.model.fc = nn.Linear(input_features_fc_layer, embedding_dimension)
+        # Output logits for softmax loss
+        self.model.classifier = nn.Linear(embedding_dimension, num_classes)
+
+    def forward(self, images):
+        """Forward pass to output the embedding vector (feature vector)."""
+        embedding = self.model(images)
+
+        return embedding
+
+    def forward_training(self, images):
+        """Forward pass during training to output both the embedding vector and logits
+          for cross entropy loss and center loss.
+        """
+        embedding = self.forward(images)
+        logits = self.model.classifier(embedding)
+
+        return embedding, logits
