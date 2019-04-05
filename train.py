a9b11bcf03b3a51df968d1fee2fc5abbdbf6169a
Tamer Thamoqa <tamerthamoqa@gmail.com>
2019-04-05 17:02:13 +0300

    Change image normalization RGB mean and std to 0.5

--- train.py
+++ train.py
@@ -47,8 +47,8 @@ def main():
         transforms.CenterCrop(size=224),
         transforms.ToTensor(),
         transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
+            mean=[0.5, 0.5, 0.5],
+            std=[0.5, 0.5, 0.5]
         )
     ])
 
