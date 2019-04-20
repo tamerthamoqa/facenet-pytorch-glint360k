d8ab995f47f5e27d4386175fa578b77493e564b5
Tamer Thamoqa <tamerthamoqa@gmail.com>
2019-04-20 12:07:28 +0300

    Modify LFWDataset.py to support .png images

--- LFWDataset.py
+++ LFWDataset.py
@@ -1,5 +1,9 @@
 """This module was imported from liorshk's 'facenet_pytorch' github repository:
-    https://github.com/liorshk/facenet_pytorch/blob/master/LFWDataset.py"""
+        https://github.com/liorshk/facenet_pytorch/blob/master/LFWDataset.py
+
+    It was modified to support lfw .png files for loading by using the code here:
+        https://github.com/davidsandberg/facenet/blob/master/src/lfw.py#L46
+"""
 
 """MIT License
 
@@ -28,19 +32,18 @@ import os
 import numpy as np
 from tqdm import tqdm
 
-class LFWDataset(datasets.ImageFolder):
-    '''
-    '''
-    def __init__(self, dir,pairs_path, transform=None):
 
-        super(LFWDataset, self).__init__(dir,transform)
+class LFWDataset(datasets.ImageFolder):
+    def __init__(self, dir, pairs_path, transform=None):
+
+        super(LFWDataset, self).__init__(dir, transform)
 
         self.pairs_path = pairs_path
 
         # LFW dir contains 2 folders: faces and lists
         self.validation_images = self.get_lfw_paths(dir)
 
-    def read_lfw_pairs(self,pairs_filename):
+    def read_lfw_pairs(self, pairs_filename):
         pairs = []
         with open(pairs_filename, 'r') as f:
             for line in f.readlines()[1:]:
@@ -48,41 +51,45 @@ class LFWDataset(datasets.ImageFolder):
                 pairs.append(pair)
         return np.array(pairs)
 
-    def get_lfw_paths(self,lfw_dir,file_ext="jpg"):
-
+    def get_lfw_paths(self, lfw_dir):
         pairs = self.read_lfw_pairs(self.pairs_path)
 
         nrof_skipped_pairs = 0
         path_list = []
         issame_list = []
-
-        for i in tqdm(range(len(pairs))):
-        #for pair in pairs:
-            pair = pairs[i]
+        for pair in pairs:
             if len(pair) == 3:
-                path0 = os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[1])+'.'+file_ext)
-                path1 = os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[2])+'.'+file_ext)
+                path0 = self.add_extension(os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[1])))
+                path1 = self.add_extension(os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[2])))
                 issame = True
             elif len(pair) == 4:
-                path0 = os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[1])+'.'+file_ext)
-                path1 = os.path.join(lfw_dir, pair[2], pair[2] + '_' + '%04d' % int(pair[3])+'.'+file_ext)
+                path0 = self.add_extension(os.path.join(lfw_dir, pair[0], pair[0] + '_' + '%04d' % int(pair[1])))
+                path1 = self.add_extension(os.path.join(lfw_dir, pair[2], pair[2] + '_' + '%04d' % int(pair[3])))
                 issame = False
-            if os.path.exists(path0) and os.path.exists(path1):    # Only add the pair if both paths exist
-                path_list.append((path0,path1,issame))
+            if os.path.exists(path0) and os.path.exists(path1):  # Only add the pair if both paths exist
+                path_list.append((path0, path1, issame))
                 issame_list.append(issame)
             else:
                 nrof_skipped_pairs += 1
-        if nrof_skipped_pairs>0:
+        if nrof_skipped_pairs > 0:
             print('Skipped %d image pairs' % nrof_skipped_pairs)
 
         return path_list
 
+    def add_extension(self, path):
+        if os.path.exists(path + '.jpg'):
+            return path + '.jpg'
+        elif os.path.exists(path + '.png'):
+            return path + '.png'
+        else:
+            raise RuntimeError('No file "%s" with extension png or jpg.' % path)
+
     def __getitem__(self, index):
-        '''
+        """
         Args:
             index: Index of the triplet or the matches - not of a single image
         Returns:
-        '''
+        """
 
         def transform(img_path):
             """Convert image into numpy array and apply transformation
@@ -93,10 +100,9 @@ class LFWDataset(datasets.ImageFolder):
             img = self.loader(img_path)
             return self.transform(img)
 
-        (path_1,path_2,issame) = self.validation_images[index]
+        (path_1, path_2, issame) = self.validation_images[index]
         img1, img2 = transform(path_1), transform(path_2)
         return img1, img2, issame
-
 
     def __len__(self):
         return len(self.validation_images)
