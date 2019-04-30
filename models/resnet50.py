814d74cad9cc5ad615436e031b4d9fe6b838bf03
Tamer Thamoqa <tamerthamoqa@gmail.com>
2019-04-30 19:20:33 +0300

    Add triplet loss versions of the resnet architectures

--- models/resnet50.py
+++ models/resnet50.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torchvision.models as models
 
 
-class Resnet50(nn.Module):
+class Resnet50Center(nn.Module):
     """Constructs a ResNet-50 model for FaceNet training using center loss with cross entropy loss.
 
     Args:
@@ -14,7 +14,7 @@ class Resnet50(nn.Module):
                            Defaults to False.
     """
     def __init__(self, num_classes, embedding_dimension=128, pretrained=False):
-        super(Resnet50, self).__init__()
+        super(Resnet50Center, self).__init__()
         self.model = models.resnet50(pretrained=pretrained)
         input_features_fc_layer = self.model.fc.in_features
         # Output embedding
@@ -41,7 +41,7 @@ class Resnet50(nn.Module):
         embedding = self.model(images)
         embedding = self.l2_norm(embedding)
         # Multiply by alpha = 10 as suggested in https://arxiv.org/pdf/1703.09507.pdf
-        #   Equation 9: number of classes in VGGFace2 = 9131
+        #   Equation 9: number of classes in VGGFace2 dataset = 9131
         #   lower bound on alpha = 5, multiply alpha by 2; alpha = 10
         alpha = 10
         embedding = embedding * alpha
@@ -49,10 +49,54 @@ class Resnet50(nn.Module):
         return embedding
 
     def forward_training(self, images):
-        """Forward pass during training to output both the embedding vector l2 norm and logits
+        """Forward pass during training to output both the l2-normed embedding vector and logits
           for cross entropy loss and center loss.
         """
         embedding = self.forward(images)
         logits = self.model.classifier(embedding)
 
         return embedding, logits
+
+
+class Resnet50Triplet(nn.Module):
+    """Constructs a ResNet-50 model for FaceNet training using triplet loss.
+
+    Args:
+        embedding_dimension (int): Required dimension of the resulting embedding layer that is outputted by the model.
+                                   using triplet loss. Defaults to 128.
+        pretrained (bool): If True, returns a model pre-trained on the ImageNet dataset from a PyTorch repository.
+                           Defaults to False.
+    """
+
+    def __init__(self, embedding_dimension=128, pretrained=False):
+        super(Resnet50Triplet, self).__init__()
+        self.model = models.resnet34(pretrained=pretrained)
+        input_features_fc_layer = self.model.fc.in_features
+        # Output embedding
+        self.model.fc = nn.Linear(input_features_fc_layer, embedding_dimension)
+
+    def l2_norm(self, input):
+        """Perform l2 normalization operation on an input vector.
+        code copied from liorshk's repository: https://github.com/liorshk/facenet_pytorch/blob/master/model.py
+        """
+        input_size = input.size()
+        buffer = torch.pow(input, 2)
+        normp = torch.sum(buffer, 1).add_(1e-10)
+        norm = torch.sqrt(normp)
+        _output = torch.div(input, norm.view(-1, 1).expand_as(input))
+        output = _output.view(input_size)
+
+        return output
+
+    def forward(self, images):
+        """Forward pass to output the embedding vector (feature vector) after l2-normalization and multiplication
+        by scalar (alpha)."""
+        embedding = self.model(images)
+        embedding = self.l2_norm(embedding)
+        # Multiply by alpha = 10 as suggested in https://arxiv.org/pdf/1703.09507.pdf
+        #   Equation 9: number of classes in VGGFace2 dataset = 9131
+        #   lower bound on alpha = 5, multiply alpha by 2; alpha = 10
+        alpha = 10
+        embedding = embedding * alpha
+
+        return embedding
