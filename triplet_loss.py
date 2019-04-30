c334303bb6bc645eb05a3a09782f7678540ad1dc
Tamer Thamoqa <tamerthamoqa@gmail.com>
2019-04-30 19:21:56 +0300

    Add imported triplet loss module and triplet loss dataloader module

---
+++ triplet_loss.py
@@ -0,0 +1,24 @@
+"""This code was imported from tbmoon's 'facenet' repository:
+    https://github.com/tbmoon/facenet/blob/master/utils.py
+"""
+
+import torch
+from torch.autograd import Function
+from torch.nn.modules.distance import PairwiseDistance
+
+
+class TripletLoss(Function):
+
+    def __init__(self, margin):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+        self.pdist = PairwiseDistance(2)
+
+    def forward(self, anchor, positive, negative):
+        pos_dist = self.pdist.forward(anchor, positive)
+        neg_dist = self.pdist.forward(anchor, negative)
+
+        hinge_dist = torch.clamp(self.margin + pos_dist - neg_dist, min=0.0)
+        loss = torch.mean(hinge_dist)
+
+        return loss
