865ec9e0b8bb2cab3c4abc1a7368a56b9f3db522
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2020-10-07 09:10:22 +0300

    Add multiprocessing for triplet generation process to speed up the generation process

--- dataloaders/triplet_loss_dataloader.py
+++ dataloaders/triplet_loss_dataloader.py
@@ -1,7 +1,8 @@
 """This code was imported from tbmoon's 'facenet' repository:
     https://github.com/tbmoon/facenet/blob/master/data_loader.py
 
-    The code was modified to support .png and .jpg files.
+    The code was modified to support multiprocessing during the triplet generation process to speed up the generation
+     process and to support .png, .jpg, and .jpeg files.
 """
 
 
@@ -9,14 +10,27 @@ import os
 import numpy as np
 import pandas as pd
 import torch
+import glob
+import multiprocessing  # For generating triplets using separate python processes because of the GIL limitation for a Python interpreter
 from tqdm import tqdm
 from PIL import Image
 from torch.utils.data import Dataset
 
 
 class TripletFaceDataset(Dataset):
-    # Modified to add 'training_triplets_path' parameter
-    def __init__(self, root_dir, csv_name, num_triplets, training_triplets_path=None, transform=None):
+    def __init__(self, root_dir, csv_name, num_triplets, num_generate_triplets_processes=0, training_triplets_path=None,
+                 transform=None):
+        """Args:
+
+        root_dir: Absolute path to dataset.
+        csv_name: Path to csv file containing the image paths inside root_dir.
+        num_triplets: Number of triplets required to be generated.
+        num_generate_triplets_processes: Number of separate Python processes to be created for the triplet generation
+                                          process. A value of 0 would generate a number of processes equal to the
+                                          number of available CPU cores.
+        training_triplets_path: Path to a pre-generated triplet numpy file to skip the triplet generation process.
+        transform: Required image transformation (augmentation) settings.
+        """
 
         # Modified here to set the data types of the dataframe columns to be suitable for other datasets other than the
         #   VggFace2 dataset (Casia-WebFace in this case because of the identities starting with numbers automatically
@@ -26,35 +40,35 @@ class TripletFaceDataset(Dataset):
         self.num_triplets = num_triplets
         self.transform = transform
 
-        # Modified here
+        if num_generate_triplets_processes == 0:
+            self.num_generate_triplets_processes = os.cpu_count()
+        else:
+            self.num_generate_triplets_processes = num_generate_triplets_processes
+
         if training_triplets_path is None:
-            self.training_triplets = self.generate_triplets(self.df, self.num_triplets)
+            self.training_triplets = self.generate_triplets(
+                df=self.df,
+                num_triplets=self.num_triplets,
+                num_generate_triplets_processes=self.num_generate_triplets_processes
+            )
         else:
             self.training_triplets = np.load(training_triplets_path)
 
-    @staticmethod
-    def generate_triplets(df, num_triplets):
+    def make_dictionary_for_face_class(self, df):
+        """
+          - face_classes = {'class0': [class0_id0, ...], 'class1': [class1_id0, ...], ...}
+        """
+        face_classes = dict()
+        for idx, label in enumerate(df['class']):
+            if label not in face_classes:
+                face_classes[label] = []
+            face_classes[label].append(df.iloc[idx, 0])
 
-        def make_dictionary_for_face_class(df):
-            """
-              - face_classes = {'class0': [class0_id0, ...], 'class1': [class1_id0, ...], ...}
-            """
-            face_classes = dict()
-            for idx, label in enumerate(df['class']):
-                if label not in face_classes:
-                    face_classes[label] = []
-                face_classes[label].append(df.iloc[idx, 0])
+        return face_classes
 
-            return face_classes
-
+    def _generate_triplets(self, df, classes, face_classes, num_triplets, process_id):
         triplets = []
-        classes = df['class'].unique()
-        face_classes = make_dictionary_for_face_class(df)
-
-        # Modified here to add a print statement
-        print("\nGenerating {} triplets...".format(num_triplets))
-
-        progress_bar = tqdm(range(num_triplets))
+        progress_bar = tqdm(range(int(num_triplets)))  # tqdm progress bar does not iterate through float numbers
         for _ in progress_bar:
 
             """
@@ -101,13 +115,78 @@ class TripletFaceDataset(Dataset):
                 ]
             )
 
-        # Modified here to save the training triplets as a numpy file to not have to redo this process every
-        #   training execution from scratch
+        np.save('datasets/temp/temp_training_triplets_{}.npy'.format(process_id), triplets)
+
+    def generate_triplets(self, df, num_triplets, num_generate_triplets_processes):
+        total_triplets = []
+        classes = df['class'].unique()
+        face_classes = self.make_dictionary_for_face_class(df)
+
+        print("\nGenerating {} triplets using {} Python processes ...".format(
+                num_triplets,
+                num_generate_triplets_processes
+            )
+        )
+
+        # If True, there are residual number of triplets to be generated after the processes are done
+        flag_residual_triplets = False
+        triplet_residual = num_triplets % num_generate_triplets_processes
+
+        if triplet_residual == 0:
+            num_triplets_per_process = num_triplets / num_generate_triplets_processes
+        else:
+            flag_residual_triplets = True
+            num_triplets_per_process = num_triplets - triplet_residual
+            num_triplets_per_process = num_triplets / num_generate_triplets_processes
+
+        processes = []
+        for i in range(num_generate_triplets_processes):
+            processes.append(multiprocessing.Process(
+                    target=self._generate_triplets,
+                    args=(df, classes, face_classes, num_triplets_per_process, i)
+                )
+            )
+
+        for process in processes:
+            process.start()
+
+        for process in processes:
+            process.join()  # Block execution until all spawned processes are done
+
+        if flag_residual_triplets:
+            print("Processes are done. Residual number of tripelts {} detected and are being generated by main process ...".format(triplet_residual))
+            self._generate_triplets(
+                df=df,
+                classes=classes,
+                face_classes=face_classes,
+                num_triplets=triplet_residual,
+                process_id=num_generate_triplets_processes + 1
+            )
+
+        numpy_files = glob.glob("datasets/temp/*.npy")
+        for numpy_file in numpy_files:
+            total_triplets.append(np.load(numpy_file))   # numpy file already contains "datasets/temp" in its path
+            os.remove(numpy_file)
+
+        # Convert total triplets list from 3D shape to 2D shape
+        total_triplets = [elem for list in total_triplets for elem in list]
+
         print("Saving training triplets list in datasets/ directory ...")
-        np.save('datasets/training_triplets_{}.npy'.format(num_triplets), triplets)
+        np.save('datasets/training_triplets_{}.npy'.format(num_triplets), total_triplets)
         print("Training triplets' list Saved!\n")
 
-        return triplets
+        return total_triplets
+
+    # Added this method to allow .jpg, .png, and .jpeg image support
+    def add_extension(self, path):
+        if os.path.exists(path + '.jpg'):
+            return path + '.jpg'
+        elif os.path.exists(path + '.png'):
+            return path + '.png'
+        elif os.path.exists(path + '.jpeg'):
+            return path + '.jpeg'
+        else:
+            raise RuntimeError('No file "{}" with extension png or jpg or jpeg.'.format(path))
 
     def __getitem__(self, idx):
 
@@ -142,12 +221,3 @@ class TripletFaceDataset(Dataset):
 
     def __len__(self):
         return len(self.training_triplets)
-
-    # Added this method to allow .jpg and .png image support
-    def add_extension(self, path):
-        if os.path.exists(path + '.jpg'):
-            return path + '.jpg'
-        elif os.path.exists(path + '.png'):
-            return path + '.png'
-        else:
-            raise RuntimeError('No file "%s" with extension png or jpg.' % path)
