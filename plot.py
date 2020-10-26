814bdaa116ddeadc6bc41215fe010f1b03e192d9
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2020-10-26 11:52:09 +0300

    Make roc plot auc value a 4 digit floating point number

--- plot.py
+++ plot.py
@@ -13,7 +13,7 @@ def plot_roc_lfw(false_positive_rate, true_positive_rate, figure_name="roc.png")
     roc_auc = auc(false_positive_rate, true_positive_rate)
     fig = plt.figure()
     plt.plot(
-        false_positive_rate, true_positive_rate, color='red', lw=2, label="ROC Curve (area = {:.2f})".format(roc_auc)
+        false_positive_rate, true_positive_rate, color='red', lw=2, label="ROC Curve (area = {:.4f})".format(roc_auc)
     )
     plt.plot([0, 1], [0, 1], color="blue", lw=2, linestyle="--", label="Random")
     plt.xlim([0.0, 1.0])
