c932b45b5784b70a841398a366cd85d65918519c
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2021-05-05 19:02:10 +0300

    Edit accuracy plot parameters in train_triplet_loss.py

--- train_triplet_loss.py
+++ train_triplet_loss.py
@@ -187,7 +187,7 @@ def set_optimizer(optimizer, model, learning_rate):
     return optimizer_model
 
 
-def validate_lfw(model, lfw_dataloader, model_architecture, epoch, epochs):
+def validate_lfw(model, lfw_dataloader, model_architecture, epoch):
     model.eval()
     with torch.no_grad():
         l2_distance = PairwiseDistance(p=2)
@@ -260,7 +260,7 @@ def validate_lfw(model, lfw_dataloader, model_architecture, epoch, epochs):
         # Plot LFW accuracies plot
         plot_accuracy_lfw(
             log_dir="logs/lfw_{}_log_triplet.txt".format(model_architecture),
-            epochs=epochs,
+            epochs=epoch,
             figure_name="plots/accuracies_plots/lfw_accuracies_{}_epoch_{}_triplet.png".format(model_architecture, epoch)
         )
     except Exception as e:
@@ -491,8 +491,7 @@ def main():
             model=model,
             lfw_dataloader=lfw_dataloader,
             model_architecture=model_architecture,
-            epoch=epoch,
-            epochs=epochs
+            epoch=epoch
         )
 
         # Save model checkpoint
